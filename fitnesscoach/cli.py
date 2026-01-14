a682955cf5edc3dfe8059b10a6262feae6868282
Alvaro Moran <alvaro.moran@huggingface.co>
2026-01-14 13:46:14 +0000

    feat(cli): nicer summary output

--- fitnesscoach/cli.py
+++ fitnesscoach/cli.py
@@ -23,10 +23,10 @@ def summary():
     sleeping_seconds = data.get("sleepingSeconds", 0)
     active_seconds = data.get("activeSeconds", 0)
 
-    print(f"Steps: {steps}")
+    print(f"Steps: {steps} ({steps / daily_step_goal * 100:.2f}%)")
     print(f"Daily Step Goal: {daily_step_goal}")
-    print(f"Active Minutes: {active_seconds // 60}")
-    print(f"Sleeping Minutes: {sleeping_seconds // 60}")
+    print(f"Active time: {active_seconds // 3600}h:{active_seconds % 3600 // 60}m")
+    print(f"Sleeping time: {sleeping_seconds // 3600}h:{sleeping_seconds % 3600 // 60}m")
 
 
 if __name__ == "__main__":
